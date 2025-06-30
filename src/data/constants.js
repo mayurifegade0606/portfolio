82b0de37ff2e015a5b07a33d8b86396c5a05ea2c
mayurifegade0606 <mayurifegade0606@gmail.com>
2025-06-29 19:29:15 -0700

    Initial commit

---
+++ src/data/constants.js
@@ -0,0 +1,246 @@
+import codeelanLogo from "../images/codeelan.PNG";
+import kasnetLogo from "../images/kasnet.png"
+import CropShield from "../images/1.Home Page.jpeg"
+import learnio from "../images/ss1 (1).png"
+import ekart from "../images/ekart.png"
+import gpj from "../images/gpj.jfif"
+import sit from "../images/sit.jfif"
+export const Bio = {
+  name: "Mayuri Fegade",
+  roles: [
+    "Full Stack Developer",
+    "Python Developer",
+    "ML Explorer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile Computer Engineering graduate with a passion for Python, AI/ML, and web development. Always eager to take on new challenges, I’ve gained hands-on experience through internships where I worked on automation testing tools and integrated ML modules. With key projects like CropShield—a crop disease detection system using TensorFlow—I am dedicated to delivering impactful, real-world solutions. With a positive attitude, strong technical skills, and a growth mindset, I am ready to contribute meaningfully and achieve great things.",
+  github: "https://github.com/mayurifegade0606/",
+  resume:
+    "https://drive.google.com/file/d/1UlfTcnxANGp9dhnwXu1yC13Y0pHFVDh-/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/mayuri-fegade-bb0370220/",
+  
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+     {
+        name: "PHP",
+        image: "https://www.vectorlogo.zone/logos/php/php-icon.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Postgresql",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+      
+    ],
+  },
+  
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image: "https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image: "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: codeelanLogo,
+    role: "Junior Developer Intern",
+    company: "CodeElan Technologies Pvt. Ltd.",
+    date: "Jan 2024 - Feb 2024",
+    desc: "Working with integrating Machine Learning libraries into Java based automation testing tool",
+    skills: [
+      "Python",
+      "OCR",
+      "OpenCV",
+      "Tesseract",
+      "PyMuPDF",
+      "Pandas",
+      "Numpy",
+     
+    ],
+    doc: "https://drive.google.com/file/d/14i2VtHJGBc6wjOA-JRWrDNd43ecayPc7/view?usp=drive_linkQ",
+  },
+  {
+    id: 0,
+    img: kasnetLogo,
+    role: "Software Engineer Intern",
+    company: "KasNet Technologies Pvt. Ltd.",
+    date: "Aug 2021 - Sep 2021",
+    desc: "CompletedatrainingprogramwithanAgrade,focusing on technologies and projects ,Java-based web development,database management.",
+    skills: [
+      "Java",
+      "Web develeopment",
+      "SDLC",
+      "Database",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    doc: "https://drive.google.com/file/d/1UQ88Lyklb9Sj4ujNT_oSHp9scyy6_7lT/view?usp=drive_link",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: sit,
+    school: "Sinhgad Institute of Technology, Lonavala",
+    date: "Aug 2022 - Jun 2025",
+    grade: "9.78 SGPA",
+    desc: "I am recent graduate with Bachelor's degree in Computer Science  at Sinhgad Institute of Technology, Lonavala. I have completed 6 semesters and have a SGPA of 9.78. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a member of the Litereray Student Club  at SIT.",
+    degree: "Bachelor of Engineering - BE, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: gpj,
+    school: "Government Polytechnic ,Jalgaon",
+    date: "Aug 2019 - Jun 2022",
+    grade: "85.20%",
+    desc: "I completed my diploma from Government Polytechnic ,Jalgaon, where I studied Computer Engineering and completed 6 semesters and acored 85.20%.",
+    degree: "Diploma, Computer Engineering",
+  },
+  
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "CropShield",
+    date: "Sep 2024 - May 2025",
+    description:
+      " Developed a real-time crop disease detection and monitoring mobile appplication. The system accurately identified diseases of crops with accuracy",
+    image:
+      CropShield,
+    tags: [
+      "Javascript",
+      "React Js",
+      "Python",
+      "Machine learning",
+      "TensorFlow",
+      "Keras",
+      "CNN",
+      "Flask",
+    ],
+    category: ["machine learning", "android app"],
+    github: "https://github.com/mayurifegade0606/CropShieldApp",
+    //webapp:
+  },
+  
+  {
+    id: 1,
+    title: "Learnio",
+    date: "Jul 2021 - May 2022",
+    description:
+      "Developed and maintained Learnio, an e-learning platform for computer science sudents. Implemented features such as interactive quizzes,progress tracking, personalized learning patterns.",
+    image:
+      learnio,
+    tags: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "PHP",
+      "MySQL",
+    ],
+    category: "web app",
+    github: "https://github.com/mayurifegade0606/E-Learning-Website",
+    // webapp: "https://vexa-app.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "E-Kart",
+    date: "Jan 2020 - May 2021",
+    description:
+      "Developed a web application containg features of login and displaying the elements to be selled using hTML, CSS, Javascript and PHP",
+    image:
+      ekart,
+    tags: ["HTML", "CSS", "Javascript", "PHP"],
+    category: "machine learning",
+    github: "https://github.com/mayurifegade0606/E-kart-Project",
+    // webapp: "https://brain-tumor.netlify.app/",
+    
+  },
+  
+];
+
+export const TimeLineData = [
+  { year: 2019, text: "Started my journey" },
+  { year: 2022, text: "Completed Diploma in Computer Engineering" },
+  { year: 2022, text: "Experties in web development" },
+  { year: 2024, text: "Explored Machine Learning and Artificial Inteligence" },
+  { year: 2025, text: "Learning Deep Learning frameworks" },
+];
